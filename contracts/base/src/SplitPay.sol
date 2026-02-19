a2ae808a9c891963614f87276b745f826a6a4d50
solidworkssa <solidworkssa@gmail.com>
2026-02-19 05:38:34 +0100

    fix: add author attribution and version to SplitPay contract

--- contracts/base/src/SplitPay.sol
+++ contracts/base/src/SplitPay.sol
@@ -2,8 +2,11 @@
 pragma solidity ^0.8.20;
 
 /// @title SplitPay Contract
+/// @author solidworkssa
 /// @notice Automated payment splitting contract.
 contract SplitPay {
+    string public constant VERSION = "1.0.0";
+
 
     function split(address[] memory _recipients) external payable {
         require(_recipients.length > 0, "No recipients");
